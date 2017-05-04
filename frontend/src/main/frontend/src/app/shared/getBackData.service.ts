db2a97e7c8bf25d0b72be19487325b57795a0bed
miles <mgeostory@gmail.com>
2017-05-05 00:21:05 +0800

    set proxy.cng.json  and get json from backend

--- frontend/src/main/frontend/src/app/shared/getBackData.service.ts
+++ frontend/src/main/frontend/src/app/shared/getBackData.service.ts
@@ -6,20 +6,19 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GetBackDataService {
-    private url = 'https://test2-25905.firebaseio.com/heros.json';  // URL to web API
-    // private url = 'http://localhost:8080/';
-    private headers:Headers =new Headers();
-    constructor(private http: Http) { 
+    // private url = 'https://test2-25905.firebaseio.com/heros.json';  // URL to web API
+    private url = '/api/greeting';
+    private headers: Headers = new Headers();
+    constructor(private http: Http) {
         // this.headers.append('Content-Type', 'application/json');
     }
     getData(): Observable<any> {
         console.log('service');
+        console.log(this.url);
         // console.log(this.http.get(this.url));
-        return this.http.get(this.url, this.headers).map((res: Response) => {
-            console.log('rr');
-            let body = res.json();
-            return body || {};
-        });
-        // .catch(this.handleError);
+        return this.http.get(this.url) // ...and calling .json() on the response to return data
+            .map((res: Response) => res.json())
+            //...errors if any
+            .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
     }
 }
