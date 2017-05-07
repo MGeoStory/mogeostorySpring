fc826cc9d653d2ed8d3b36168913e8d4974a4539
miles <mgeostory@gmail.com>
2017-05-07 11:02:13 +0800

    server log by sfj4

--- backend/src/main/java/com/mgstory/GreetingController.java
+++ backend/src/main/java/com/mgstory/GreetingController.java
@@ -5,14 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 public class GreetingController {
-
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/api/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        logger.info("greeting");
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
