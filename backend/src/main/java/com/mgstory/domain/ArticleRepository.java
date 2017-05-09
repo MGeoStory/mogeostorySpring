23353a034c0dfd470c3312eb43641853cc11b7aa
miles <mgeostory@gmail.com>
2017-05-10 00:26:19 +0800

    add ArticleController for RepositoryRestResource

--- backend/src/main/java/com/mgstory/domain/ArticleRepository.java
+++ backend/src/main/java/com/mgstory/domain/ArticleRepository.java
@@ -1,6 +1,5 @@
 package com.mgstory.domain;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
@@ -8,15 +7,12 @@ import java.util.List;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.prepost.PreAuthorize;
 
-
 @RepositoryRestResource(collectionResourceRel = "article", path = "article")
 public interface ArticleRepository extends PagingAndSortingRepository<Article, Integer> {
     
-    //http://localhost:8080/api/article/search/kind?k=test
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    //http://localhost:8080/article/search/kind?k=test == api/article/search/kind/test
     @RestResource(path = "kind", rel = "findByKind")
     public List<Article> findByKind(@Param("k") String kind);
-
     // Prevents POST /people and PATCH /people/:id
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @Override
--- backend/src/main/java/com/mgstory/web/ArticleController.java
+++ backend/src/main/java/com/mgstory/web/ArticleController.java
@@ -1,8 +1,7 @@
 package com.mgstory.web;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,34 +12,33 @@ import com.mgstory.domain.ArticleRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
 
-
-// @RestController
-// @RequestMapping(value = "api/article")
+@RestController
+@RequestMapping(value = "api/article")
 public class ArticleController {
 
-    // private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    ArticleRepository repository;
 
-    // @Autowired
-    // ArticleReopsitory repository;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    // Article article = new Article();
+    Article article = new Article();
 
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public Iterable<Article> findAll() {
+        // logger.info("getArticles");
+        // logger.info(repository.findAll());
+        return repository.findAll();
+    }
 
-    // @RequestMapping(value = "", method = RequestMethod.GET)
-    // public Article getArticles() {
-    //     //TO DO...
-    //     logger.info("getArticles");
-    //     logger.info(repository.findAll().toString());
-    //     return article;
-    // }
+    @RequestMapping(value="/search/kind/{kind}", method = RequestMethod.GET)
+    public List<Article> findByKind(@PathVariable String kind) {
+        return repository.findByKind(kind);
+    }
 
-
-    // @RequestMapping(value="/{id}", method = RequestMethod.GET, produces = "application/json")
-    // public Article getArticle(@PathVariable int id) {
-    //     //TO DO...
-    //     logger.info("find by id: " + id);
-    //     logger.info(repository.findOne(id).toString());
-    //     return article;
-    // }
+    @RequestMapping(value="/search/id/{id}", method = RequestMethod.GET)
+    public Article findByKind(@PathVariable Integer id) {
+        return repository.findOne(id);
+    }
 }
