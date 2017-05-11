a6a8aea226629cafeff3063630337d5f975628f7
miles <mgeostory@gmail.com>
2017-05-11 20:19:35 +0800

    change folder structure of backend

--- backend/src/main/java/com/mgstory/repository/ArticleRepository.java
+++ backend/src/main/java/com/mgstory/repository/ArticleRepository.java
@@ -1,11 +1,13 @@
-package com.mgstory.domain;
+package com.mgstory.repository;
 
+import com.mgstory.domain.Article;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import java.util.List;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.prepost.PreAuthorize;
+
 
 //https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.at-query
 //Rel is to reference the collection of database
--- backend/src/main/java/com/mgstory/web/ArticleController.java
+++ backend/src/main/java/com/mgstory/web/ArticleController.java
@@ -8,14 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.mgstory.domain.Article;
-import com.mgstory.domain.ArticleRepository;
+import com.mgstory.repository.ArticleRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 @RestController
-@RequestMapping(value = "api/article")
+// @RequestMapping(value = "api/article")
 public class ArticleController {
 
     @Autowired
@@ -32,12 +32,12 @@ public class ArticleController {
         return repository.findAll();
     }
 
-    @RequestMapping(value="/search/kind/{kind}", method = RequestMethod.GET)
+    // @RequestMapping(value="/search/kind/{kind}", method = RequestMethod.GET)
     public List<Article> findByKind(@PathVariable String kind) {
         return repository.findByKind(kind);
     }
 
-    @RequestMapping(value="/search/id/{id}", method = RequestMethod.GET)
+    // @RequestMapping(value="/search/id/{id}", method = RequestMethod.GET)
     public Article findByKind(@PathVariable Integer id) {
         return repository.findOne(id);
     }
