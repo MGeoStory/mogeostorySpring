0f242f528391b7280bb401ace8fb54787935d82b
miles <mgeostory@gmail.com>
2017-05-20 19:20:31 +0800

    find by years from postDI

--- backend/src/main/java/com/mgstory/repository/PostDiRepository.java
+++ backend/src/main/java/com/mgstory/repository/PostDiRepository.java
@@ -1,18 +1,18 @@
 package com.mgstory.repository;
 
 import com.mgstory.domain.PostDi;
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
+import java.util.List;
+import org.springframework.data.repository.query.Param;
 
 @RepositoryRestResource(collectionResourceRel = "postdi", path = "postdi")
 public interface PostDiRepository extends CrudRepository<PostDi, Integer> {
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @Override
-    @RestResource(exported = false)
+    @RestResource(path = "year", rel = "findByYear")
+    List<PostDi> findByYears(@Param("y") Integer year);
     PostDi save(PostDi a);
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
