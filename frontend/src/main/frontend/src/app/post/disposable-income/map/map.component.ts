05fa24d9584074e73fa66a45c1a334aeb28c06f8
miles <mgeostory@gmail.com>
2017-05-23 21:59:27 +0800

    reset the color of feature and dont need rendering map made by geojson

--- frontend/src/main/frontend/src/app/post/disposable-income/map/map.component.ts
+++ frontend/src/main/frontend/src/app/post/disposable-income/map/map.component.ts
@@ -7,9 +7,10 @@ import * as L from 'leaflet';
 
 const MAP_ID: string = 'lmap';
 const GEOJSON_DATA: string = 'assets/geodata/county_tw-ms.json';
-const mc: MapComponent = this;
 let map: L.Map;
 let layersOfCounty: L.GeoJSON;
+let isFirstLoading: boolean = true;
+let colorizeFeatures: d3.ScaleLinear<any, any>;
 
 @Component({
   selector: 'post-disposable-income-map',
@@ -19,58 +20,107 @@ let layersOfCounty: L.GeoJSON;
 })
 
 export class MapComponent implements OnInit {
-  // map: L.Map;
-  // private layersOfCounty: L.GeoJSON;
-
-
   constructor(private lms: LMapSettingService, private os: ObservableService) {
   }
 
   ngOnInit() {
     d3.select('#leaf-map').attr('id', MAP_ID);
     map = this.lms.initMap(MAP_ID);
-    map.addControl(this.lms.addIconOfResetControll()).on('click', this.resetControl);
+    console.log(isFirstLoading);
     this.os.observedData.subscribe(
-      (data: Object[]) => {
-        console.log(data);
+      (dbData: Object[]) => {
+        //use a if to avoid async error caused by mappingTwiwanByCOunty(d3) and subscribe. 
+        if (isFirstLoading) {
+          colorizeFeatures = this.linearColorize(dbData);
+          this.mappingTaiwanByCounty(dbData);
+          isFirstLoading = false;
+        } else {
+          this.resetLayersStyle(dbData);
+
+        }
       }
     );
-
-    this.mappingTaiwanByCounty();
+    map.addControl(this.lms.addIconOfResetControll()).on('click', this.resetControl);
   }
+
+  /**
+   * use d3.extent and d3.scaleLinear to produce a colorize tool for features by value of each feature.
+   */
+  linearColorize(dbData: Object[]) {
+    let values: number[] = [];
+    for (let a in dbData[0]) {
+      if (a != 'twall' && a != 'year' && a != '_links') {
+        // console.log(data[0]);
+        values.push(dbData[0][a]);
+      }
+    }
+    return d3.scaleLinear<string>()
+      .domain(d3.extent(values))
+      .range(["#FFEDA0", "#800026"]);
+  }
+
+  /**
+   * reset the color of feature and dont need render the geoJson
+   * @param dbData 
+   */
+  resetLayersStyle(dbData: Object[]) {
+    layersOfCounty.setStyle((feature) => {
+      return { fillColor: this.getFillColor(feature.properties["COUNTYID"], dbData) }
+    });
+  }
+
+  /**
+   * mapping Taiwan By county only once that user entre page in first time.
+   */
+  mappingTaiwanByCounty(dbData: Object[]): void {
+    console.log("mappingTaiwanByCounty");
+    d3.json(GEOJSON_DATA, (geoData) => {
+      layersOfCounty = L.geoJSON(geoData, {
+        style: (feature) => {
+          // console.log(feature.properties["COUNTYID"]);
+          return {
+            fillColor: this.getFillColor(feature.properties["COUNTYID"], dbData),
+            fillOpacity: 0.9,
+            color: 'gray',
+            dashArray: '3',
+            weight: 1.5
+          }
+        }
+      }).addTo(map);
+
+      // this.setEachLayers(layersOfCounty, dbData);
+      map.fitBounds(layersOfCounty.getBounds());
+    });
+
+  }// .. mappingTaiwanByCounty
+
+
+  /**
+   * get the filled color by id of geojson and value from database
+   * @param countyId 
+   * @param dbData 
+   */
+  getFillColor(countyId: string, dbData: Object[]) {
+    // console.log(countyId);
+    // console.log(dbData[0][countyId.toLowerCase()]);
+    let valueOfCountry: number;
+    if (dbData[0][countyId.toLowerCase()] != null) {
+      valueOfCountry = dbData[0][countyId.toLowerCase()];
+      return colorizeFeatures(valueOfCountry);
+      // val colorizeFeatures(valueOfCountry);
+    } else {
+      // valueOfCountry = 0;
+      return ('LightGrey');
+    }
+  }
+
+  /**
+   * zoom to bounds that contain geojson
+   */
   resetControl() {
     console.log("resetControl");
     console.log(typeof map);
     map.fitBounds(layersOfCounty.getBounds());
-  }
-
-  mappingTaiwanByCounty(): void {
-    console.log("mappingTaiwanByCounty");
-    d3.json(GEOJSON_DATA, function (data) {
-      console.log(data);
-
-      layersOfCounty = L.geoJSON(data, {
-        style: (feature) => {
-          return { color: 'red' }
-        }
-      }).addTo(map);
-
-      layersOfCounty.setStyle(() => {
-        return { color: 'grey' }
-      });
-
-      layersOfCounty.eachLayer((layer) => {
-        // layer.bindPopup('hello');
-        // layer.bindTooltip('111');
-      }).on({
-        click: function (e) {
-          console.log('clicked');
-          console.log(e);
-        }
-      });
-
-      map.fitBounds(layersOfCounty.getBounds());
-    });
   }
 }//.. MapComponent
 
