a6e7f2b31cc09d31bc0d3549a23dcc6576ac3790
miles <mgeostory@gmail.com>
2017-05-25 00:04:37 +0800

    draw column graph of di-post

--- frontend/src/main/frontend/src/app/post/disposable-income/bar-graph/bar-graph.component.ts
+++ frontend/src/main/frontend/src/app/post/disposable-income/bar-graph/bar-graph.component.ts
@@ -22,7 +22,6 @@ export class BarGraphComponent implements OnInit {
 
     ngOnInit() {
         isFirstLoading = true;
-        canvas = gc.createCanvas('bar-canvas', '#bar-graph');
 
         this.obs.observedString.subscribe(
             (userClicked: string[]) => {
@@ -31,22 +30,88 @@ export class BarGraphComponent implements OnInit {
         )
         this.obs.observedData.subscribe(
             (dbData: Object[]) => {
-                if (isFirstLoading) {
-                    valueOfCounty = this.simplifiedDbData(dbData);
-                    this.drawColumnGraph(valueOfCounty);
-                    isFirstLoading = false;
-                } else {
+                //remove the old graph
+                if (d3.select('#bar-graph').empty()) {
+                    console.log(d3.select('#bar-graph').empty());
                 }
-
+                this.graphTitle = `${dbData[0]['year']}年-各縣市平均每戶可支配所得(萬元)：`; 
+                console.log();
+                canvas = gc.createCanvas('bar-canvas', '#bar-graph');
+                valueOfCounty = this.simplifiedDbData(dbData);
+                this.drawColumnGraph(valueOfCounty);
             }
         )
     }//.. ngOnInit
 
+
+    /**
+     * draw bar, value, text, axis
+     * @param valueOfCounty 
+     */
     drawColumnGraph(valueOfCounty: Object[]): void {
         console.log(valueOfCounty);
-        let maxOfData = valueOfCounty[0]['value'];
-        let names;
 
+        //return a ['name','name'....];
+        gc.xScaleBand.domain(
+            valueOfCounty.map((d) => {
+                return d['name']
+            })
+        )
+
+        // valueOfCounty[0]['value'] is the max value
+        gc.yScaleLinear.domain([0, valueOfCounty[0]['value']]);
+
+        //append bar chart
+        canvas.selectAll('rect').data(valueOfCounty).enter().append('rect')
+            .attr('x', (d) => gc.xScaleBand(d['name']))
+            .attr('y', (d) => gc.yScaleLinear(d['value']))
+            .attr('class', (d) => (d['name']))
+            .attr('width', gc.xScaleBand.bandwidth())
+            .attr('height', (d) => gc.getFrameHeight() - gc.yScaleLinear(d['value']))
+            .attr('fill', 'skyblue');
+
+        //append values of bar
+        canvas.selectAll('text').data(valueOfCounty).enter().append('text')
+            .attr('class', 'bar-value')
+            .attr('x', (d) => gc.xScaleBand(d['name']) + gc.xScaleBand.bandwidth() / 2)
+            .attr('y', (d) => gc.yScaleLinear(d['value']) - 5)
+            .attr('text-anchor', 'middle')
+            .text((d) => d3.format(".1f")(d['value'] / 10000))
+            //0.9em will equal 0.9 x the parent font-size
+            .style('font-size', '1rem')
+            .style('fill', (d, i) => {
+                if (i % 2 == 0) {
+                    return 'black';
+                } else {
+                    return 'DimGrey';
+                }
+            });
+
+        //the text of domain name from gc.scaleBand.domain()
+        let textOfAaxis = canvas.append('g')
+            .attr('class', 'xAxis')
+            .attr('transform', `translate(0,${gc.getFrameHeight()})`)
+            .call(gc.xAxisOfBand())
+            .selectAll('text')
+            .style('fill', (d, i) => {
+                if (i % 2 == 0) {
+                    return 'black';
+                } else {
+                    return 'DimGrey';
+                }
+            });
+
+        //make text more reabable
+        textOfAaxis.attr('transform', 'rotate(45)')
+            .attr('x', 20)
+            .style('font-size', '1rem');
+
+
+        // canvas.append('circle')
+        //     .attr('class', 'info')
+        //     .attr('cx', gc.getFrameWidth()*0.9)
+        //     .attr('cy', 0)
+        //     .attr('r', 20);
 
     }//.. drawColumnGraph
 
@@ -68,5 +133,5 @@ export class BarGraphComponent implements OnInit {
             return d3.descending(x['value'], y['value']);
         })
         return newObject;
-    }
+    }//.. simplifiedDbData
 }
