efa9342d4097c78a787aeded04f1fc08935e2360
miles <mgeostory@gmail.com>
2017-05-27 00:55:48 +0800

    build war for tomcat (default)

--- backend/src/main/java/com/mgstory/MgsApplication.java
+++ backend/src/main/java/com/mgstory/MgsApplication.java
@@ -2,9 +2,16 @@ package com.mgstory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class MgsApplication {
+public class MgsApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MgsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MgsApplication.class, args);
