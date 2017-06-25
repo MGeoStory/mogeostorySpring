4de4d3b136bd158f8a84ad35e6ab62235366ca0f
miles <mgeostory@gmail.com>
2017-06-25 15:50:36 +0800

    table worked with bug (if the data.length > endnumber will not update correctly)

--- frontend/src/main/frontend/src/app/post/earthquake/table/table.component.ts
+++ frontend/src/main/frontend/src/app/post/earthquake/table/table.component.ts
@@ -11,39 +11,63 @@ import * as d3 from 'd3';
 })
 
 export class TableComponent implements OnInit {
-    private graphTitle: string = `在地圖上點選縣市即可查看各縣市調查資料！`;
-
+    private tableTitle: string = `在地圖上點選縣市即可查看各縣市調查資料！`;
+    // private table = d3.select('#earthquake-table').append('table').attr('class', 'eTable');
+    // private thead = this.table.append('thead');
+    // private tbody = this.table.append('tbody');
+    private columns = ['地震編號', '發生日期', '規模', '深度（km）', '震央位置'];
+    private rowsName = ['code', 'date', 'scale', 'deep', 'center'];
 
     // private showTableIs: boolean = false;
 
     constructor(private os: ObservableService, private es: EarthquakeService) { }
 
     ngOnInit() {
+
+
         this.os.observedData.subscribe(
             (data) => {
                 console.log(data);
-
-                let table = d3.select('#earthquake-table').append('table');
-                let thead = table.append('thead');
-                let tbody = table.append('tbody');
-                let columns = ['地震編號', '發生日期', '規模', '深度', '震央位置'];
-                thead.append("tr")
-                    .selectAll("th")
-                    .data(columns)
-                    .enter()
-                    .append("th")
-                    .text((d) => {
-                        return d;
-                    });
-
-                let rows = tbody.selectAll('tr')
-                    .data(data).enter().append('tr').text((d) => {
-                        console.log(d);
-                        return '';
-                    });
+                if (d3.select('.eTable').empty()) {
+                    console.log(d3.select('.eTable').empty());
+                    this.drawTable(data);
+                } else {
+                    d3.select('.eTable').remove();
+                    this.drawTable(data);
+                }
+                this.tableTitle = `七大地震(規模)資訊摘要：`;
+            });
+    }
 
 
-                this.graphTitle = `在地圖上點選縣市即可檢視各縣市家戶所得分配情形！`;
+
+    drawTable(data) {
+        let table = d3.select('#earthquake-table').append('table').attr('class', 'eTable');
+        let thead = table.append('thead');
+        let tbody = table.append('tbody');
+        thead.append("tr")
+            .selectAll("th")
+            .data(this.columns)
+            .enter()
+            .append("th")
+            .text((d) => {
+                return d;
+            });
+        let rows = tbody.selectAll('tr')
+            .data(data).enter().append('tr');
+
+        let cells = rows.selectAll('td')
+            // translate json to key,vlaue
+            .data((rows) => {
+                return this.rowsName.map((column) => {
+                    return { column: column, value: rows[column] }
+                })
+            })
+            .enter()
+            .append('td')
+            .text((d) => {
+                // console.log(d);
+                return d.value
             });
     }
 }
