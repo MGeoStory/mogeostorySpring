3cb58f5e2e156dfb4b6125fa606f505351f2aed4
miles <mgeostory@gmail.com>
2017-06-29 22:17:45 +0800

    artilce of earthquake is done.

--- frontend/src/main/frontend/src/app/post/earthquake/earthquake.component.ts
+++ frontend/src/main/frontend/src/app/post/earthquake/earthquake.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticleControllerService } from 'app/services/backend/articlesController.service';
+import * as d3 from 'd3';
 
 @Component({
     selector: 'post-earthquake',
@@ -6,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class EarthquakeComponent implements OnInit {
-    constructor() { }
+    title: string;
+    date: string;
+    author: string;
+    constructor(private acs: ArticleControllerService) { }
 
-    ngOnInit() { }
+    ngOnInit() {
+        this.acs.getArticleById(3).subscribe(
+            article => {
+                console.log(article["title"]);
+                this.title = article["title"];
+                let t = d3.timeFormat("%Y/%m/%d");
+                this.date = t(article["postDate"]);
+                this.author = article["author"];
+            });
+
+    }
 }
